7fb2578142843c9c486038be813b7b5070ccbd01
Lisa Zangrando <lisa.zangrando@pd.infn.it>
2017-05-22 12:23:38 +0200

    Missing security support
    This fix provides the security support to Synergy.
    
    Bug: #1691352
    Change-Id: I2535b2a3edeea5e56cd8918d01070a6f8a534c3e
    Sem-Ver: bugfix

--- synergy/client/keystone_v3.py
+++ synergy/client/keystone_v3.py
@@ -208,6 +208,11 @@ class KeystoneClient(object):
 
         self.token = Token(token_subject, token_data)
 
+        return self.token
+
+    def getToken(self):
+        return self.token
+
     def getService(self, name):
         for service in self.token.getCatalog():
             if service["name"] == name:
